d7d2c18aa231d685a5a83b7e8d471c28749a22b3
Roman Karki <romankarki2052@gmail.com>
2023-09-10 16:23:39 +0545

    fix: logic to register the new user is added

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -9,7 +9,28 @@ export const authUser = async (req, res) => {
 // route    POST /api/users
 // @access  Public
 export const registerUser = async (req, res) => {
-  res.status(200).json({ message: "Register User" });
+  const { name, email, password } = req.body;
+  const userExist = User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data");
+  }
 };
 
 // @desc    Logout user
